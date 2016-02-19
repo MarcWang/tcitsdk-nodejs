c67b47e08a0546b6717f3104331f0247695308d5
MarcWang <s4161035@gmail.com>
2016-02-19 16:31:58 +0800

    ADD unit test

--- test.js
+++ test.js
@@ -1,14 +1,61 @@
-var TCITLocalApi = require('./index.js');
+var expect = require('chai').expect;
+var assert = require('chai').assert;
 var fs = require("fs");
 
-var localapiController = new TCITLocalApi();
+describe('Test API of TCIT LocalAPI SDK', function() {
+    var TCITLocalApi = require('./index.js');
+    var buffer = null;
+    it('load', function() {
 
-if (!localapiController.setServerInfo('localhost', 8800)) {
-    console.log("Set Server Information Error");
-} else {
-    console.log("Set Server Information Success"); 
-}
+    });
 
-process.on('SIGINT', function() {
-    process.exit();
+    before(function() {
+        // 任何需要在測試前執行的程式
+        buffer = fs.readFileSync('./image/lena.jpg');
+    });
+    after(function() {
+        // 任何需要在測試後刪除的資料
+    });
+
+    describe('API.getVersion', function() {
+        var localapiController = new TCITLocalApi();
+        it('should be return string', function() {
+            expect(localapiController.getVersion()).to.be.a('string');
+        });
+    });
+
+    describe('API.setServerInfo', function() {
+        var localapiController = new TCITLocalApi();
+        it('should be return false when no value input, ', function() {
+            expect(localapiController.setServerInfo()).to.be.equal(false);
+        });
+        it('should be return true when set host, port and wsport,', function() {
+            expect(localapiController.setServerInfo('localhost', 8800, 4662)).to.be.equal(true);
+        });
+        it('should be return false when set port and wsport with non-number type,', function() {
+            expect(localapiController.setServerInfo('localhost', "8800", "4662")).to.be.equal(false);
+        });
+    });
+
+    describe('API.imageBufferUpload', function() {
+        var localapiController = new TCITLocalApi();
+
+        it('should be return img_id when input buffer of image', function() {
+            var result = localapiController.imageBufferUpload(buffer);
+            return result.then(function(res) {
+                expect(res).to.have.a.property('img_id');
+            }, function(err) {
+                assert.fail(err);
+            });
+        });
+
+        it('should be a string for img_id type', function() {
+            var result = localapiController.imageBufferUpload(buffer);
+            return result.then(function(res) {
+                expect(res.img_id).to.have.a('string');
+            }, function(err) {
+                assert.fail(err);
+            });
+        });
+    });
 });
